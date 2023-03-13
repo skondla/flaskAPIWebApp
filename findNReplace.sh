80af565c00665bd209607cb37992f6eab1a5e1c4
skondla <kondlawork@gmail.com>
2023-03-13 10:02:47 -0400

    Update findNReplace.sh

--- findNReplace.sh
+++ findNReplace.sh
@@ -17,11 +17,3 @@ find . -type f -name "*.sh" -print0 | xargs -0 sed -i '' -e 's/myDB/newDB/g'
 #find . -type f -name "cron*.*" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
 #find . -type f -name "*.sql" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
 #find . -type f -name "README.md" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.sh" -print0 | xargs -0 sed -i '' -e 's/ckbgoobdxe24/net1/g'
-#find . -type f -name "*.py" -print0 | xargs -0 sed -i '' -e 's/ckbgoobdxe24/net1/g'
-#find . -type f -name "*.sh*" -print0 | xargs -0 sed -i '' -e 's/ckbgoobdxe24/net1/g'
-#find . -type f -name "*.sh" -print0 | xargs -0 sed -i '' -e 's/sudheer.kondla\@gmail.com/skondla\@me.com/g'
-
-#multiple files
-
-#find . -type f \( -name "*.sh" -o -name "*.py" \) | xargs grep -i "example.notifyme.com"
