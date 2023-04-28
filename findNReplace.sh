4ce7d9c90a805933e48cd8f1c4f0d5d4ac47ef70
skondla <7891645+skondla@users.noreply.github.com>
2023-04-27 22:02:34 -0400

    modified findNReplace.sh

--- findNReplace.sh
+++ findNReplace.sh
@@ -3,17 +3,3 @@
 #Purpose: Find and replace a string recursively in all directories under current directory
 
 find . -type f -name "*.sh" -print0 | xargs -0 sed -i '' -e 's/myDB/newDB/g'
-
-
-#find . -type f -name "*.sh" | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.sh" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.py" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.sh" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.org" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.old" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.sh*" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.cron*" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "cron.*" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "cron*.*" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "*.sql" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
-#find . -type f -name "README.md" -print0 | xargs -0 sed -i '' -e 's/mydb1/myDB/g'
