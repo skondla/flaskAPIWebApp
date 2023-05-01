1c4ddab4c2794ca49f33f78990e6f78880ef9f86
skondla <7891645+skondla@users.noreply.github.com>
2023-04-30 21:15:09 -0400

    modified findNReplace.sh to add regex

--- findNReplace.sh
+++ findNReplace.sh
@@ -3,3 +3,6 @@
 #Purpose: Find and replace a string recursively in all directories under current directory
 
 find . -type f -name "*.sh" -print0 | xargs -0 sed -i '' -e 's/myDB/newDB/g'
+#find -E . -regex '.*\.(py|sh|sql|md)' | xargs -0 sed -i '' -e 's/myDB/newDB/g'
+#find -E . -regex '.*\.(py|sh|sql|md)' |xargs egrep -i "(yahoo.com|me.com|gmail.com)"
+
