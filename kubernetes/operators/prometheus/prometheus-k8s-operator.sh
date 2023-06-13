40b5efa4e34358980c01521c428e2ccbf7aa235b
skondla <kondlawork@gmail.com>
2023-06-12 22:59:16 -0400

    Add rabbitmq , prometheus k8s operators

---
+++ kubernetes/operators/prometheus/prometheus-k8s-operator.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.24.0/install.sh | bash -s v0.24.0
+kubectl create -f https://operatorhub.io/install/prometheus.yaml
+kubectl get csv -n operators
---
+++ kubernetes/operators/rabbitmq/rabbitmq-k8s-operator.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
